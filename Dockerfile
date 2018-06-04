ffa2924cdffe886c72db70701b937094855e1ca8
Jan Rotter <jan.rotter@ocado.com>
2018-06-04 10:11:02 +0200

    Make the docker container usage more idiomatic.
    Currently it is impossible to change the working directory
    of the container without breaking its operation. Running:
    
    docker run -it -v somevolume:/somewhere -w /somewhere ...
    
    will make the pipenv invocation invalid and requires the user
    to provide correct groovy scripts folder path. This is a common
    scenario when the container is used with Jenkins or Gitlab, where
    by default the working directory is set to the mounted workspace.
    
    This commit resolves both of these issues, i.e. removes the dependency
    on the pipenv inside the container and sets an absolute path
    to the groovy scripts directory as a default.
    
    Note:
    The alpine python image was replaced with a slim one due
    to the following errors during the build:
    
    Processing /app
    Could not install packages due to an EnvironmentError: [('/app/nexus_configurator/groovy',
    '/tmp/pip-req-build-n_d4zqs0/nexus_configurator/groovy',
    "[Errno 95] Not supported: '/tmp/pip-req-build-n_d4zqs0/nexus_configurator/groovy'")]

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
-FROM python:alpine3.6
+FROM python:3.6-slim
 
 COPY . /app
 WORKDIR /app
-RUN pip install pipenv && pipenv install
-ENV SHELL=/bin/sh
-ENTRYPOINT ["pipenv", "run", "nexus_configurator"]
+RUN pip install .
+USER nobody
+ENTRYPOINT ["nexus_configurator"]
--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 
 release="v2.1.2"
 rm -rf nexus3-*
+rm -rf nexus_configurator/groovy
 curl -L https://github.com/ansible-ThoTeam/nexus3-oss/archive/${release}.tar.gz | tar xz
 mv nexus3-* nexus3-oss
-ln -s nexus3-oss/files/groovy/ groovy
+ln -s ../nexus3-oss/files/groovy nexus_configurator/groovy
